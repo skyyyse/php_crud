96db492bd9e3ab288a6fa170d4a3bed58121ea5a
[Skyyyes] <[skyyy.gaming122@gmail.com]>
2024-05-17 13:47:12 +0700

    sql

--- sql.sql
+++ sql.sql
@@ -1,21 +1,21 @@
 create database information_student;
 CREATE TABLE `student` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL PRIMARY KEY,
   `name` varchar(100) DEFAULT NULL,
   `sex` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `school` int(10) DEFAULT NULL,
   `subject` varchar(100) DEFAULT NULL,
   `date` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 CREATE TABLE `school` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL PRIMARY KEY,
   `title_school` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 CREATE TABLE `subject` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `title_subject` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 INSERT INTO `student` (`id`, `name`, `sex`, `email`, `school`, `subject`, `date`) VALUES
 (1, 'test', 'male', 'test@gmail.com', 1, '1', '2024-05-04'),
@@ -54,48 +54,3 @@ INSERT INTO `subject` (`id`, `title_subject`) VALUES
 (10, 'Video Eediter'),
 (11, 'Digital Content Officer'),
 (12, 'Other');
-
-
-ALTER TABLE `school`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `student`
---
-ALTER TABLE `student`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `subject`
---
-ALTER TABLE `subject`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `school`
---
-ALTER TABLE `school`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `student`
---
-ALTER TABLE `student`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `subject`
---
-ALTER TABLE `subject`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
